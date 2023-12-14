c96c69e9261cd7ad23dc2f4eb4c114b24815dcbf
Petr Zelina <zelina.pe@gmail.com>
2023-12-14 10:19:30 +0100

    day 14 numba: 320x faster

--- py/task14.py
+++ py/task14.py
@@ -1,5 +1,6 @@
 from task import Task
 import numpy as np
+from numba import jit, njit
 
 
 ROLL  = 79
@@ -26,25 +27,40 @@ def get_iter(arr, dr, dc):
     if dc == -1:
         return ((r, c) for r in range(mr) for c in range(mc-1, -1, -1))
 
+@njit
+def get_nb_iter(iters, dr, dc) -> np.array:
+    if dr == 1:
+        return iters[0]
+    if dr == -1:
+        return iters[1]
+    if dc == 1:
+        return iters[2]
+    if dc == -1:
+        return iters[3]
+
+@njit
 def get_roll_slice(r, c, dr, dc, gathered_rocks):
     if dr == 1:
-        return slice(r-gathered_rocks, r), slice(c, c+1)
+        return r-gathered_rocks, r, c, c+1
     if dr == -1:
-        return slice(r+1, r+gathered_rocks+1), slice(c, c+1)
+        return r+1, r+gathered_rocks+1, c, c+1
     if dc == 1:
-        return slice(r, r+1), slice(c-gathered_rocks, c)
+        return r, r+1, c-gathered_rocks, c
     if dc == -1:
-        return slice(r, r+1), slice(c+1, c+gathered_rocks+1)
+        return r, r+1, c+1, c+gathered_rocks+1
 
-def roll_rocks(arr, dr, dc):
-    it = get_iter(arr, dr, dc)
+@njit
+def roll_rocks(arr, dr, dc, iters):
+    it = get_nb_iter(iters, dr, dc)
 
     gathered_rocks = 0
     for r, c in it:
         cur = arr[r, c]
         if cur == STOP:
-            sr, sc = get_roll_slice(r, c, dr, dc, gathered_rocks)
-            arr[sr, sc] = ROLL
+            if gathered_rocks == 0:
+                continue
+            sr1, sr2, sc1, sc2 = get_roll_slice(r, c, dr, dc, gathered_rocks)
+            arr[sr1:sr2, sc1:sc2] = ROLL
             gathered_rocks = 0
         elif cur == ROLL:
             gathered_rocks += 1
@@ -63,16 +79,16 @@ def load(arr, dr, dc):
         return ((arr == ROLL) * np.arange(mc)[None, ::-1]).sum()
 
 # part 2
-def cycle(arr):
+def cycle(arr, iters):
     for dr, dc in ((-1,0), (0,-1), (1,0), (0,1)):
-        roll_rocks(arr, dr, dc)
+        roll_rocks(arr, dr, dc, iters)
 
-def skip_periodic(arr, N, prev, i):
+def skip_periodic(arr, N, prev, i, iters):
     remaining = N-i
     period = i - prev
     to_make = remaining % period
     for _ in range(to_make):
-        cycle(arr)
+        cycle(arr, iters)
 
 # debug
 def print_arr(arr):
@@ -82,26 +98,33 @@ def print_arr(arr):
 def part1(text, timer):
     arr = parse(text)
     timer.parsed()
-    # print(arr)
+    iters = tuple(
+        np.array(list(get_iter(arr, dr, dc)))
+        for dr, dc in ((1,0), (-1,0), (0,1), (0,-1))
+    )
     dr, dc = -1, 0
-    arr = roll_rocks(arr, dr, dc)
-    # print(arr)
+    arr = roll_rocks(arr, dr, dc, iters)
     return load(arr, dr, dc)
+
 
 def part2(text, timer):
     arr = parse(text)
     timer.parsed()
+    iters = tuple(
+        np.array(list(get_iter(arr, dr, dc)))
+        for dr, dc in ((1,0), (-1,0), (0,1), (0,-1))
+    )
     N = 1000000000
     cache = {}
     cache[arr.tobytes()] = 0
     for i in range(N):
-        cycle(arr)
+        cycle(arr, iters)
         i += 1
         rep = arr.tobytes()
         prev = cache.get(rep)
         if prev is not None:
             # print("hit", prev, i)
-            skip_periodic(arr, N, prev, i)
+            skip_periodic(arr, N, prev, i, iters)
             break
         cache[rep] = i
     return load(arr, -1, 0)
@@ -115,5 +138,5 @@ task = Task(
 
 if __name__ == "__main__":
     task.run()
-    task.benchmark(2)
+    task.benchmark(10)
 
